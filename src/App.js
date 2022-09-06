09c1327a4e91594834cbd2db1dbeee993f8d4077
SergioV <sergioscode@gmail.com>
2022-09-05 17:37:57 -0700

    git pushing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
 <div className="App" style={{backgroundColor: '#181821', height: '100vh'}}>
-     
+     <h1 className=' text-2xl text-white'>Hello</h1>
     </div>
   );
 }
